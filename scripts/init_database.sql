3084770a57d3fdab90213997ca6e1e707b139919
sridhar2362000 <63249840+sridhar2362000@users.noreply.github.com>
2025-09-19 11:52:52 +0530

    Add files via upload

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -3,13 +3,26 @@ Create Database and schemas :
 This scripts create the new database named as 'DataWarehouse' and schemas like 'bronze', 'silver', 'gold'
 */
 
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+GO
+-- Create the 'DataWarehouse' database
+CREATE DATABASE DataWarehouse;
+GO
 
+USE DataWarehouse;
+GO
 
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
 
-create database DataWarehouse;
+CREATE SCHEMA silver;
+GO
 
-use DataWarehouse;
-
-create schema bronze;
-create schema silver;
-create schema gold;
+CREATE SCHEMA gold;
+GO
